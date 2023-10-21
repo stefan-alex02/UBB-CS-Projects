202d9fb36097de74a247332d5e00827d602cab88
Ştefan Alexandrescu <stefan.alexandrescu2002@gmail.com>
2023-10-21 17:37:01 +0300

    Main : updated Lab 1 project for Retele de calculatoare

--- RETELE DE CALCULATOARE/Lab 1/Lab-1-Client-Java/src/Main.java
+++ RETELE DE CALCULATOARE/Lab 1/Lab-1-Client-Java/src/Main.java
@@ -1,17 +1,92 @@
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void main(String args[]) {
+        if (args.length != 2) {
+            System.err.println("Invalid number of arguments.\nUsage: ./[file] [SERVER_ADDRESS] [PORT]");
+            return;
+        }
+        String SERVER_ADDRESS = args[0];
+        if (!SERVER_ADDRESS.matches(
+                "^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]).){3}" +
+                        "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$")) {
+            System.err.println("The first argument is not a valid server address.");
+            return;
+        }
+        int SERVER_PORT = Integer.parseInt(args[1]);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+        Socket socket = null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            String s1 = readInputString("Type 1st string : ", reader);
+            String s2 = readInputString("Type 2nd string : ", reader);
+            writeStringsToSocket(s1, s2, socket);
+            readAndHandleResponseFromSocket(socket);
+        } catch (IOException e) {
+            System.err.println("Caught exception " + e.getMessage());
+        } finally {
+            closeStreams(socket,reader);
+        }
+    }
+
+    private static void readAndHandleResponseFromSocket(Socket c) throws IOException {
+        DataInputStream socketIn = new DataInputStream(c.getInputStream());
+        int statusCode = socketIn.readUnsignedShort();
+        System.out.println("> Received status code : " + statusCode);
+        switch (statusCode) {
+            case 200:
+                System.out.println("> Result string is : " + readStringFromStream(socketIn));
+                break;
+            case 401:
+                System.out.println("Error : input strings must not be empty.");
+                break;
+            default:
+                System.out.println("An unknown error occurred.");
+                break;
+        }
+    }
+
+    private static String readStringFromStream(DataInputStream inputStream) throws IOException {
+        int length = inputStream.readShort();
+        return new String(inputStream.readNBytes(length), StandardCharsets.UTF_8);
+    }
+
+    private static void writeStringsToSocket(String s1, String s2, Socket c) throws IOException {
+        DataOutputStream socketOut = new DataOutputStream(c.getOutputStream());
+        socketOut.writeShort(s1.length());
+        socketOut.write(s1.getBytes());
+        socketOut.writeShort(s2.length());
+        socketOut.write(s2.getBytes());
+        socketOut.flush();
+    }
+
+    private static String readInputString(String message, BufferedReader reader) throws IOException {
+        System.out.print(message);
+        return reader.readLine();
+    }
+
+    private static void closeStreams(Socket socket, BufferedReader reader) {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Could not close socket!");
+            }
+        }
+        if (reader != null) {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                System.err.println("Could not close reader!");
+            }
         }
     }
 }
---
+++ RETELE DE CALCULATOARE/Lab 1/Lab-1-Client-Server-C/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Stefan/UBB-CS-Projects/RETELE DE CALCULATOARE/Lab 1/Lab-1-Client-Server-C/client.c" "CMakeFiles/Client.dir/client.c.o" "gcc" "CMakeFiles/Client.dir/client.c.o.d"
+  "/mnt/c/Users/Stefan/UBB-CS-Projects/RETELE DE CALCULATOARE/Lab 1/Lab-1-Client-Server-C/common.c" "CMakeFiles/Client.dir/common.c.o" "gcc" "CMakeFiles/Client.dir/common.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
