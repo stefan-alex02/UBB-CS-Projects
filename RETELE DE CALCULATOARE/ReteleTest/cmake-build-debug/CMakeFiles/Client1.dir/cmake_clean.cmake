84b17e6cfa5afb639d32cbedaf8838cd8c0b0a63
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2023-11-09 14:42:54 +0200

    CompNetworks : added Lab Test project.

---
+++ RETELE DE CALCULATOARE/ReteleTest/cmake-build-debug/CMakeFiles/Client1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client1.dir/client.c.o"
+  "CMakeFiles/Client1.dir/client.c.o.d"
+  "Client1"
+  "Client1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Client1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
