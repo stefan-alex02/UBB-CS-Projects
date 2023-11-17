652d2ae64c7aa704ba2980b560769e5586437bca
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2023-11-17 11:18:46 +0200

    MAP : updated DB tables.

--- MAP/Laboratoare/DBSocialNetwork/CreateTables.sql
+++ MAP/Laboratoare/DBSocialNetwork/CreateTables.sql
@@ -5,38 +5,49 @@
 --     CONNECTION LIMIT = -1
 --     IS_TEMPLATE = False;
 
-DROP TABLE IF EXISTS friendships;  
-DROP TABLE IF EXISTS users;  
 
-CREATE TABLE public.users
+
+-- Table: public.users
+
+-- DROP TABLE IF EXISTS public.users;
+
+CREATE TABLE IF NOT EXISTS public.users
 (
-    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY,
-    first_name character varying,
-    last_name character varying,
-    PRIMARY KEY (id)
-);
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    first_name character varying COLLATE pg_catalog."default",
+    last_name character varying COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.users
     OWNER to postgres;
+	
+	
+	
+-- Table: public.friendships
 
-CREATE TABLE public.friendships
+-- DROP TABLE IF EXISTS public.friendships;
+
+CREATE TABLE IF NOT EXISTS public.friendships
 (
     id1 bigint NOT NULL,
     id2 bigint NOT NULL,
-    friends_from timestamp WITHOUT time zone NOT NULL,
+    friends_from timestamp without time zone NOT NULL,
     CONSTRAINT pk_idpair PRIMARY KEY (id1, id2),
     CONSTRAINT fk_id1 FOREIGN KEY (id1)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE CASCADE
-        NOT VALID,
+        ON DELETE CASCADE,
     CONSTRAINT fk_id2 FOREIGN KEY (id2)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
-        ON DELETE CASCADE
-        NOT VALID,
+        ON DELETE CASCADE,
     CONSTRAINT ascending_pair CHECK (id1 < id2)
-);
+)
+
+TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.friendships
     OWNER to postgres;
