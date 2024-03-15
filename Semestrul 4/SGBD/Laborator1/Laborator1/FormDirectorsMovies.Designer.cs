238ea2ccd318d94ee2b550b34efa47c6f9c2f5aa
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2024-03-15 12:24:54 +0200

    SGBD : finished Lab1

--- Semestrul 4/SGBD/Laborator1/Laborator1/FormDirectorsMovies.Designer.cs
+++ Semestrul 4/SGBD/Laborator1/Laborator1/FormDirectorsMovies.Designer.cs
@@ -32,7 +32,6 @@ partial class FormDirectorsMovies {
         labelMovieDirectorID = new Label();
         textBoxMovieDirectorID = new TextBox();
         labelMovieLaunchDate = new Label();
-        textBoxMovieLaunchDate = new TextBox();
         labelMovieRunningTime = new Label();
         textBoxMovieRunningTime = new TextBox();
         buttonAddMovie = new Button();
@@ -40,6 +39,7 @@ partial class FormDirectorsMovies {
         buttonDeleteMovie = new Button();
         labelDirectors = new Label();
         labelMovies = new Label();
+        dateTimePickerMovieLaunchDate = new DateTimePicker();
         ((System.ComponentModel.ISupportInitialize)dataGridViewDirectors).BeginInit();
         ((System.ComponentModel.ISupportInitialize)dataGridViewMovies).BeginInit();
         SuspendLayout();
@@ -51,13 +51,17 @@ partial class FormDirectorsMovies {
         dataGridViewDirectors.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
         dataGridViewDirectors.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
         dataGridViewDirectors.EditMode = DataGridViewEditMode.EditProgrammatically;
-        dataGridViewDirectors.Location = new Point(24, 193);
+        dataGridViewDirectors.Location = new Point(34, 322);
+        dataGridViewDirectors.Margin = new Padding(4, 5, 4, 5);
+        dataGridViewDirectors.MaximumSize = new Size(500, 400);
+        dataGridViewDirectors.MinimumSize = new Size(200, 350);
         dataGridViewDirectors.MultiSelect = false;
         dataGridViewDirectors.Name = "dataGridViewDirectors";
+        dataGridViewDirectors.RowHeadersWidth = 62;
         dataGridViewDirectors.RowTemplate.Height = 25;
-        dataGridViewDirectors.Size = new Size(353, 225);
+        dataGridViewDirectors.Size = new Size(500, 350);
         dataGridViewDirectors.TabIndex = 0;
-        dataGridViewDirectors.CellMouseClick += dataGridViewDirectors_CellMouseClick;
+        dataGridViewDirectors.CellClick += dataGridViewDirectors_CellClick;
         // 
         // dataGridViewMovies
         // 
@@ -66,11 +70,13 @@ partial class FormDirectorsMovies {
         dataGridViewMovies.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
         dataGridViewMovies.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
         dataGridViewMovies.EditMode = DataGridViewEditMode.EditProgrammatically;
-        dataGridViewMovies.Location = new Point(409, 93);
+        dataGridViewMovies.Location = new Point(584, 155);
+        dataGridViewMovies.Margin = new Padding(4, 5, 4, 5);
         dataGridViewMovies.MultiSelect = false;
         dataGridViewMovies.Name = "dataGridViewMovies";
+        dataGridViewMovies.RowHeadersWidth = 62;
         dataGridViewMovies.RowTemplate.Height = 25;
-        dataGridViewMovies.Size = new Size(357, 325);
+        dataGridViewMovies.Size = new Size(510, 542);
         dataGridViewMovies.TabIndex = 1;
         dataGridViewMovies.CellClick += dataGridViewMovies_CellClick;
         // 
@@ -78,104 +84,114 @@ partial class FormDirectorsMovies {
         // 
         labelMovieTitle.AccessibleName = "";
         labelMovieTitle.AutoSize = true;
-        labelMovieTitle.Location = new Point(24, 27);
+        labelMovieTitle.Location = new Point(34, 45);
+        labelMovieTitle.Margin = new Padding(4, 0, 4, 0);
         labelMovieTitle.Name = "labelMovieTitle";
-        labelMovieTitle.Size = new Size(66, 15);
+        labelMovieTitle.Size = new Size(99, 25);
         labelMovieTitle.TabIndex = 2;
         labelMovieTitle.Text = "Movie title:";
         // 
         // textBoxMovieTitle
         // 
-        textBoxMovieTitle.Location = new Point(145, 24);
+        textBoxMovieTitle.Location = new Point(207, 40);
+        textBoxMovieTitle.Margin = new Padding(4, 5, 4, 5);
         textBoxMovieTitle.Name = "textBoxMovieTitle";
-        textBoxMovieTitle.Size = new Size(132, 23);
+        textBoxMovieTitle.Size = new Size(187, 31);
         textBoxMovieTitle.TabIndex = 3;
         // 
         // labelMovieDirectorID
         // 
         labelMovieDirectorID.AccessibleName = "";
         labelMovieDirectorID.AutoSize = true;
-        labelMovieDirectorID.Location = new Point(24, 60);
+        labelMovieDirectorID.Location = new Point(34, 100);
+        labelMovieDirectorID.Margin = new Padding(4, 0, 4, 0);
         labelMovieDirectorID.Name = "labelMovieDirectorID";
-        labelMovieDirectorID.Size = new Size(101, 15);
+        labelMovieDirectorID.Size = new Size(154, 25);
         labelMovieDirectorID.TabIndex = 4;
         labelMovieDirectorID.Text = "Movie director ID:";
         // 
         // textBoxMovieDirectorID
         // 
-        textBoxMovieDirectorID.Location = new Point(145, 57);
+        textBoxMovieDirectorID.Enabled = false;
+        textBoxMovieDirectorID.Location = new Point(207, 95);
+        textBoxMovieDirectorID.Margin = new Padding(4, 5, 4, 5);
         textBoxMovieDirectorID.Name = "textBoxMovieDirectorID";
-        textBoxMovieDirectorID.Size = new Size(132, 23);
+        textBoxMovieDirectorID.Size = new Size(187, 31);
         textBoxMovieDirectorID.TabIndex = 5;
         // 
         // labelMovieLaunchDate
         // 
         labelMovieLaunchDate.AccessibleName = "";
         labelMovieLaunchDate.AutoSize = true;
-        labelMovieLaunchDate.Location = new Point(24, 93);
+        labelMovieLaunchDate.Location = new Point(34, 155);
+        labelMovieLaunchDate.Margin = new Padding(4, 0, 4, 0);
         labelMovieLaunchDate.Name = "labelMovieLaunchDate";
-        labelMovieLaunchDate.Size = new Size(108, 15);
+        labelMovieLaunchDate.Size = new Size(161, 25);
         labelMovieLaunchDate.TabIndex = 6;
         labelMovieLaunchDate.Text = "Movie launch date:";
-        // 
-        // textBoxMovieLaunchDate
-        // 
-        textBoxMovieLaunchDate.Location = new Point(145, 90);
-        textBoxMovieLaunchDate.Name = "textBoxMovieLaunchDate";
-        textBoxMovieLaunchDate.Size = new Size(132, 23);
-        textBoxMovieLaunchDate.TabIndex = 7;
         // 
         // labelMovieRunningTime
         // 
         labelMovieRunningTime.AccessibleName = "";
         labelMovieRunningTime.AutoSize = true;
-        labelMovieRunningTime.Location = new Point(24, 128);
+        labelMovieRunningTime.Location = new Point(34, 213);
+        labelMovieRunningTime.Margin = new Padding(4, 0, 4, 0);
         labelMovieRunningTime.Name = "labelMovieRunningTime";
-        labelMovieRunningTime.Size = new Size(115, 15);
+        labelMovieRunningTime.Size = new Size(171, 25);
         labelMovieRunningTime.TabIndex = 8;
         labelMovieRunningTime.Text = "Movie running time:";
         // 
         // textBoxMovieRunningTime
         // 
-        textBoxMovieRunningTime.Location = new Point(145, 125);
+        textBoxMovieRunningTime.Location = new Point(207, 208);
+        textBoxMovieRunningTime.Margin = new Padding(4, 5, 4, 5);
         textBoxMovieRunningTime.Name = "textBoxMovieRunningTime";
-        textBoxMovieRunningTime.Size = new Size(132, 23);
+        textBoxMovieRunningTime.Size = new Size(187, 31);
         textBoxMovieRunningTime.TabIndex = 9;
         // 
         // buttonAddMovie
         // 
-        buttonAddMovie.Location = new Point(339, 19);
+        buttonAddMovie.Location = new Point(484, 32);
+        buttonAddMovie.Margin = new Padding(4, 5, 4, 5);
         buttonAddMovie.Name = "buttonAddMovie";
-        buttonAddMovie.Size = new Size(108, 30);
+        buttonAddMovie.Size = new Size(154, 50);
         buttonAddMovie.TabIndex = 10;
         buttonAddMovie.Text = "Add movie";
         buttonAddMovie.UseVisualStyleBackColor = true;
+        buttonAddMovie.Click += buttonAddMovie_Click;
         // 
         // buttonUpdateMovie
         // 
-        buttonUpdateMovie.Location = new Point(498, 19);
+        buttonUpdateMovie.Enabled = false;
+        buttonUpdateMovie.Location = new Point(711, 32);
+        buttonUpdateMovie.Margin = new Padding(4, 5, 4, 5);
         buttonUpdateMovie.Name = "buttonUpdateMovie";
-        buttonUpdateMovie.Size = new Size(108, 30);
+        buttonUpdateMovie.Size = new Size(154, 50);
         buttonUpdateMovie.TabIndex = 11;
         buttonUpdateMovie.Text = "Update movie";
         buttonUpdateMovie.UseVisualStyleBackColor = true;
+        buttonUpdateMovie.Click += buttonUpdateMovie_Click;
         // 
         // buttonDeleteMovie
         // 
-        buttonDeleteMovie.Location = new Point(658, 19);
+        buttonDeleteMovie.Enabled = false;
+        buttonDeleteMovie.Location = new Point(940, 32);
+        buttonDeleteMovie.Margin = new Padding(4, 5, 4, 5);
         buttonDeleteMovie.Name = "buttonDeleteMovie";
-        buttonDeleteMovie.Size = new Size(108, 30);
+        buttonDeleteMovie.Size = new Size(154, 50);
         buttonDeleteMovie.TabIndex = 12;
         buttonDeleteMovie.Text = "Delete movie";
         buttonDeleteMovie.UseVisualStyleBackColor = true;
+        buttonDeleteMovie.Click += buttonDeleteMovie_Click;
         // 
         // labelDirectors
         // 
         labelDirectors.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
         labelDirectors.AutoSize = true;
-        labelDirectors.Location = new Point(168, 175);
+        labelDirectors.Location = new Point(240, 292);
+        labelDirectors.Margin = new Padding(4, 0, 4, 0);
         labelDirectors.Name = "labelDirectors";
-        labelDirectors.Size = new Size(54, 15);
+        labelDirectors.Size = new Size(83, 25);
         labelDirectors.TabIndex = 13;
         labelDirectors.Text = "Directors";
         // 
@@ -183,17 +199,28 @@ partial class FormDirectorsMovies {
         // 
         labelMovies.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
         labelMovies.AutoSize = true;
-        labelMovies.Location = new Point(561, 75);
+        labelMovies.Location = new Point(801, 125);
+        labelMovies.Margin = new Padding(4, 0, 4, 0);
         labelMovies.Name = "labelMovies";
-        labelMovies.Size = new Size(45, 15);
+        labelMovies.Size = new Size(69, 25);
         labelMovies.TabIndex = 14;
         labelMovies.Text = "Movies";
         // 
+        // dateTimePickerMovieLaunchDate
+        // 
+        dateTimePickerMovieLaunchDate.CustomFormat = "MMMM dd, yyyy";
+        dateTimePickerMovieLaunchDate.Location = new Point(207, 149);
+        dateTimePickerMovieLaunchDate.Name = "dateTimePickerMovieLaunchDate";
+        dateTimePickerMovieLaunchDate.Size = new Size(231, 31);
+        dateTimePickerMovieLaunchDate.TabIndex = 15;
+        dateTimePickerMovieLaunchDate.Value = new DateTime(2024, 3, 15, 11, 10, 28, 0);
+        // 
         // FormDirectorsMovies
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(10F, 25F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(1143, 750);
+        Controls.Add(dateTimePickerMovieLaunchDate);
         Controls.Add(labelMovies);
         Controls.Add(labelDirectors);
         Controls.Add(buttonDeleteMovie);
@@ -201,7 +228,6 @@ partial class FormDirectorsMovies {
         Controls.Add(buttonAddMovie);
         Controls.Add(textBoxMovieRunningTime);
         Controls.Add(labelMovieRunningTime);
-        Controls.Add(textBoxMovieLaunchDate);
         Controls.Add(labelMovieLaunchDate);
         Controls.Add(textBoxMovieDirectorID);
         Controls.Add(labelMovieDirectorID);
@@ -209,6 +235,7 @@ partial class FormDirectorsMovies {
         Controls.Add(labelMovieTitle);
         Controls.Add(dataGridViewMovies);
         Controls.Add(dataGridViewDirectors);
+        Margin = new Padding(4, 5, 4, 5);
         Name = "FormDirectorsMovies";
         Text = "Directors & Movies";
         Load += Form1_Load;
@@ -227,7 +254,6 @@ partial class FormDirectorsMovies {
     private Label labelMovieDirectorID;
     private TextBox textBoxMovieDirectorID;
     private Label labelMovieLaunchDate;
-    private TextBox textBoxMovieLaunchDate;
     private Label labelMovieRunningTime;
     private TextBox textBoxMovieRunningTime;
     private Button buttonAddMovie;
@@ -235,4 +261,5 @@ partial class FormDirectorsMovies {
     private Button buttonDeleteMovie;
     private Label labelDirectors;
     private Label labelMovies;
+    private DateTimePicker dateTimePickerMovieLaunchDate;
 }
--- Semestrul 4/SGBD/Laborator1/Laborator1/FormDirectorsMovies.cs
+++ Semestrul 4/SGBD/Laborator1/Laborator1/FormDirectorsMovies.cs
@@ -1,11 +1,16 @@
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace Laborator1;
 
 public partial class FormDirectorsMovies : Form {
-    SqlConnection cs = new SqlConnection("Data Source=DESKTOP-HH5PFSL\\SQLEXPRESS;Initial Catalog=Cinematography;Integrated Security=True");
+    //const string dbDataSource = "DESKTOP-HH5PFSL\\SQLEXPRESS";
+    const string dbDataSource = "DESKTOP-0SVDHT0\\SQLEXPRESS";
+
+    SqlConnection cs = new SqlConnection("Data Source=" + dbDataSource + 
+        ";Initial Catalog=Cinematography;Integrated Security=True");
 
     SqlDataAdapter directorsAdapter = new SqlDataAdapter();
     SqlDataAdapter moviesAdapter = new SqlDataAdapter();
@@ -17,16 +22,21 @@ public partial class FormDirectorsMovies : Form {
 
     public FormDirectorsMovies() {
         InitializeComponent();
+        dateTimePickerMovieLaunchDate.Format = DateTimePickerFormat.Custom;
+    }
+
+    private void ReloadContent() {
+        directorsAdapter.SelectCommand = new SqlCommand("select * from Directors", cs);
+        moviesAdapter.SelectCommand = new SqlCommand("select * from Movies", cs);
+
+        ds.Clear();
+        directorsAdapter.Fill(ds, "directors");
+        moviesAdapter.Fill(ds, "movies");
     }
 
     private void Form1_Load(object sender, EventArgs e) {
         try {
-            directorsAdapter.SelectCommand = new SqlCommand("select * from Directors", cs);
-            moviesAdapter.SelectCommand = new SqlCommand("select * from Movies", cs);
-
-            ds.Clear();
-            directorsAdapter.Fill(ds, "directors");
-            moviesAdapter.Fill(ds, "movies");
+            ReloadContent();
 
             directorsBS.DataSource = ds.Tables["directors"];
             dataGridViewDirectors.DataSource = directorsBS;
@@ -47,24 +57,161 @@ public partial class FormDirectorsMovies : Form {
         }
     }
 
-    private void dataGridViewDirectors_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e) {
+    public void ClearTextBoxes() {
+        textBoxMovieTitle.Text = String.Empty;
+        textBoxMovieDirectorID.Text = String.Empty;
+        dateTimePickerMovieLaunchDate.Value = DateTime.Today;
+        textBoxMovieRunningTime.Text = String.Empty;
+
+        textBoxMovieDirectorID.Enabled = false;
+        buttonUpdateMovie.Enabled = false;
+        buttonDeleteMovie.Enabled = false;
+    }
+
+    private void dataGridViewDirectors_CellClick(object sender, DataGridViewCellEventArgs e) {
+        ClearTextBoxes();
+
+        if (dataGridViewDirectors.SelectedCells.Count == 0 || e.RowIndex < 0) {
+            return;
+        }
+
         DataGridViewRow selectedRow = dataGridViewDirectors.Rows[e.RowIndex];
         int directorID = Convert.ToInt32(selectedRow.Cells["DirectorID"].Value);
 
         moviesBS.Filter = $"DirectorID = {directorID}";
-
-        textBoxMovieTitle.Text = String.Empty;
-        textBoxMovieDirectorID.Text = String.Empty;
-        textBoxMovieLaunchDate.Text = String.Empty;
-        textBoxMovieRunningTime.Text = String.Empty;
     }
 
     private void dataGridViewMovies_CellClick(object sender, DataGridViewCellEventArgs e) {
+        if (dataGridViewMovies.SelectedCells.Count == 0 || e.RowIndex < 0) {
+            ClearTextBoxes();
+            return;
+        }
+
+        textBoxMovieDirectorID.Enabled = true;
+        buttonUpdateMovie.Enabled = true;
+        buttonDeleteMovie.Enabled = true;
+
         DataGridViewRow selectedRow = dataGridViewMovies.Rows[e.RowIndex];
 
         textBoxMovieTitle.Text = Convert.ToString(selectedRow.Cells["Title"].Value);
         textBoxMovieDirectorID.Text = Convert.ToString(selectedRow.Cells["DirectorID"].Value);
-        textBoxMovieLaunchDate.Text = Convert.ToString(selectedRow.Cells["LaunchDate"].Value);
+        dateTimePickerMovieLaunchDate.Value = Convert.ToDateTime(selectedRow.Cells["LaunchDate"].Value);
         textBoxMovieRunningTime.Text = Convert.ToString(selectedRow.Cells["RunningTime"].Value);
+    }
+
+    private void buttonAddMovie_Click(object sender, EventArgs e) {
+        if (dataGridViewDirectors.SelectedCells.Count <= 0) {
+            MessageBox.Show("You must select a director!");
+            return;
+        }
+        int directorID = Convert.ToInt32(dataGridViewDirectors
+            .SelectedCells[0]
+            .OwningRow
+            .Cells["DirectorID"]
+            .Value);
+
+        moviesAdapter.InsertCommand = new SqlCommand("insert into Movies values " +
+            "(@title, @directorID, @launchDate, @runningTime)", cs);
+
+        moviesAdapter.InsertCommand.Parameters.Add("@title", SqlDbType.VarChar).Value = 
+            textBoxMovieTitle.Text;
+        moviesAdapter.InsertCommand.Parameters.Add("@directorID", SqlDbType.Int).Value =
+            directorID;
+        moviesAdapter.InsertCommand.Parameters.Add("@launchDate", SqlDbType.Date).Value = 
+            dateTimePickerMovieLaunchDate.Value;
+        moviesAdapter.InsertCommand.Parameters.Add("@runningTime", SqlDbType.Int).Value = 
+            textBoxMovieRunningTime.Text;
+
+        try {
+            cs.Open();
+            moviesAdapter.InsertCommand.ExecuteNonQuery();
+            cs.Close();
+
+            ReloadContent();
+        }
+        catch (Exception ex) {
+            MessageBox.Show(ex.Message);
+            cs.Close();
+        }
+    }
+
+    private void buttonUpdateMovie_Click(object sender, EventArgs e) {
+        if (dataGridViewMovies.SelectedCells.Count < 1) {
+            MessageBox.Show("You must select a record to update!");
+            return;
+        }
+
+        int movieID = Convert.ToInt32(dataGridViewMovies
+            .SelectedCells[0]
+            .OwningRow
+            .Cells["MovieID"]
+            .Value);
+
+        moviesAdapter.UpdateCommand = new SqlCommand("update Movies set " +
+            "Title = @title, DirectorID = @directorID, " +
+            "LaunchDate = @launchDate, RunningTime = @runningTime " +
+            "where MovieID = @movieID", cs);
+
+        moviesAdapter.UpdateCommand.Parameters.Add("@title", SqlDbType.VarChar).Value =
+            textBoxMovieTitle.Text;
+        moviesAdapter.UpdateCommand.Parameters.Add("@directorID", SqlDbType.Int).Value =
+            textBoxMovieDirectorID.Text;
+        moviesAdapter.UpdateCommand.Parameters.Add("@launchDate", SqlDbType.Date).Value =
+            dateTimePickerMovieLaunchDate.Value;
+        moviesAdapter.UpdateCommand.Parameters.Add("@runningTime", SqlDbType.Int).Value =
+            textBoxMovieRunningTime.Text;
+
+        moviesAdapter.UpdateCommand.Parameters.Add("@movieID", SqlDbType.Int).Value =
+            movieID;
+
+        try {
+            cs.Open();
+            int result = moviesAdapter.UpdateCommand.ExecuteNonQuery();
+            cs.Close();
+
+            if (result < 1) {
+                MessageBox.Show("Failed to update movie record");
+            }
+
+            ReloadContent();
+        }
+        catch (Exception ex) {
+            MessageBox.Show(ex.Message);
+            cs.Close();
+        }
+    }
+
+    private void buttonDeleteMovie_Click(object sender, EventArgs e) {
+        if (dataGridViewMovies.SelectedCells.Count < 1) {
+            MessageBox.Show("You must select a record to delete!");
+            return;
+        }
+
+        int movieID = Convert.ToInt32(dataGridViewMovies.SelectedCells[0].OwningRow.Cells["MovieID"].Value);
+
+
+        if (MessageBox.Show("Are you sure you want to delete?",
+            "Confirm Deletion",
+            MessageBoxButtons.YesNo) == DialogResult.No) {
+            return;
+        }
+
+        moviesAdapter.DeleteCommand = new SqlCommand("delete from Movies where MovieID = @movieID", cs);
+
+        moviesAdapter.DeleteCommand.Parameters.Add("@movieID", SqlDbType.Int).Value = movieID;
+
+        try {
+            cs.Open();
+            int result = moviesAdapter.DeleteCommand.ExecuteNonQuery();
+            cs.Close();
+
+            ReloadContent();
+
+            dataGridViewMovies.ClearSelection();
+        }
+        catch (Exception ex) {
+            MessageBox.Show(ex.Message);
+            cs.Close();
+        }
     }
 }
