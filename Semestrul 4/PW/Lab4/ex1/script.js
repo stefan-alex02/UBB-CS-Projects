16f877a3a395b418490bb80e42f89a26c8ec260c
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2024-04-11 22:59:10 +0300

    Small changes

---
+++ Semestrul 4/PW/Lab4/ex1/script.js
@@ -0,0 +1,24 @@
+selects = document.getElementsByTagName("select");
+console.log(selects);
+for (let i = 0; i < selects.length; i++) {
+    console.log(selects[i]);
+
+    selects[i].nextList = selects[(i+1)%selects.length];
+    updateSize(selects[i]);
+    for (let option of selects[i].options) {
+            option.ondblclick = function() {
+                parent = this.parentElement;
+                nextParent = parent.nextList;
+
+                parent.removeChild(this);
+                nextParent.appendChild(this);
+
+                updateSize(parent);
+                updateSize(nextParent);
+            };
+    }
+}
+
+function updateSize(select) {
+    select.size = select.length;
+}
