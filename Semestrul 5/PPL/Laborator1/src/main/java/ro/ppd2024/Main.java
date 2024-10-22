dc080c70eeccfe6142fc03a052b03c3abc8a9d62
stefan-alex02 <43996943+stefan-alex02@users.noreply.github.com>
2024-10-22 19:48:48 +0300

    PPD - additions to lab1

--- Semestrul 5/PPL/Laborator1/src/main/java/ro/ppd2024/Main.java
+++ Semestrul 5/PPL/Laborator1/src/main/java/ro/ppd2024/Main.java
@@ -8,12 +8,10 @@ import ro.ppd2024.util.ResultComparer;
 import ro.ppd2024.util.SequentialConvolution;
 import ro.ppd2024.util.Technique;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import static ro.ppd2024.MatrixFileHandler.readDataFromFile;
 
 public class Main {
-    public static long performTimer(Runnable runnable, int n, int m) {
+    public static long performTimer(Runnable runnable) {
         long start = System.nanoTime();
         runnable.run();
         long end = System.nanoTime();
@@ -21,7 +19,7 @@ public class Main {
     }
 
     public static long performTimerAndAssertion(Runnable runnable, int[][] expected, int[][] actual, int n, int m) {
-        long time = performTimer(runnable, n, m);
+        long time = performTimer(runnable);
 //        printMatrix(expected, n, m);
 //        System.out.println();
 //        printMatrix(actual, n, m);
@@ -40,39 +38,8 @@ public class Main {
         }
     }
 
-    public static DataSuite readDataFromFile(String filename) {
-        DataSuite dataSuite = new DataSuite();
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
-            String line = bufferedReader.readLine();
-            String[] tokens = line.split(" ");
-            dataSuite.n = Integer.parseInt(tokens[0]);
-            dataSuite.m = Integer.parseInt(tokens[1]);
-            dataSuite.k = Integer.parseInt(tokens[2]);
-            dataSuite.F = new int[dataSuite.n][dataSuite.m];
-            dataSuite.C = new int[dataSuite.k][dataSuite.k];
-            for (int i = 0; i < dataSuite.n; i++) {
-                line = bufferedReader.readLine();
-                tokens = line.split(" ");
-                for (int j = 0; j < dataSuite.m; j++) {
-                    dataSuite.F[i][j] = Integer.parseInt(tokens[j]);
-                }
-            }
-            for (int i = 0; i < dataSuite.k; i++) {
-                line = bufferedReader.readLine();
-                tokens = line.split(" ");
-                for (int j = 0; j < dataSuite.k; j++) {
-                    dataSuite.C[i][j] = Integer.parseInt(tokens[j]);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return dataSuite;
-    }
-
     public static void main(String[] args) {
-        DataSuite suite = readDataFromFile("src/main/resources/data_1000_1000_5.txt");
+        DataSuite suite = readDataFromFile("src/main/resources/input/data_10_10_3.txt");
 //        suite.nrThreads = Integer.parseInt(args[1]);
 //        suite.technique = Technique.values()[Integer.parseInt(args[2])];
         suite.nrThreads = Integer.parseInt("8");
@@ -82,8 +49,7 @@ public class Main {
         int[][] VParallel = new int[suite.n][suite.m];
 
         long sequentialTime = performTimer(() ->
-                        SequentialConvolution.performTask(suite.F, suite.n, suite.m, VSequential, suite.C, suite.k),
-                suite.n, suite.m);
+                SequentialConvolution.performTask(suite.F, suite.n, suite.m, VSequential, suite.C, suite.k));
 
 //            System.out.println(technique.name());
 
@@ -102,10 +68,13 @@ public class Main {
                             suite.k, suite.nrThreads, suite.technique), VSequential, VParallel, suite.n, suite.m);
 
             System.out.println(suite.technique.name() + " - Seq: " + sequentialTime + " Par: " + parallelTime);
+
+            MatrixFileHandler.printResultToFile(VParallel, suite.n, suite.m,
+                    "src/main/resources/output/result_" + suite.n + "_" + suite.m + "_" + suite.k + ".txt");
         }
     }
 
-    static class DataSuite {
+    public static class DataSuite {
         int[][] F;
         int[][] C;
         int n;
---
+++ Semestrul 5/PPL/Laborator1/src/main/java/ro/ppd2024/MatrixFileHandler.java
@@ -0,0 +1,97 @@
+package ro.ppd2024;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MatrixFileHandler {
+    public static int[][] generate(int n, int m, int max, int min) {
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = (int) (Math.random() * (max - min + 1) + min);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void writeMatrixToFile(FileWriter fileWriter, int[][] matrix, int n, int m) throws IOException {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                fileWriter.write(matrix[i][j] + " ");
+            }
+            fileWriter.write("\n");
+        }
+    }
+
+    public static void readMatrixFromFile(BufferedReader bufferedReader, int[][] matrix, int n, int m) throws IOException {
+        for (int i = 0; i < n; i++) {
+            String line = bufferedReader.readLine();
+            String[] tokens = line.split(" ");
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = Integer.parseInt(tokens[j]);
+            }
+        }
+    }
+
+    public static void generateFile(int n, int m, int k, int max, int min) {
+        int[][] matrix = generate(n, m, max, min);
+        int[][] filter = generate(k, k, max, min);
+
+        // writing to file with format "data_[n]_[m]_[k].txt", in resources folder
+        try {
+            FileWriter fileWriter =
+                    new FileWriter("src/main/resources/data_" + n + "_" + m + "_" + k + ".txt");
+            fileWriter.write(n + " " + m + " " + k + "\n");
+            writeMatrixToFile(fileWriter, matrix, n, m);
+            writeMatrixToFile(fileWriter, filter, k, k);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printResultToFile(int[][] result, int n, int m, String filename) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename);
+            fileWriter.write(n + " " + m + "\n");
+            writeMatrixToFile(fileWriter, result, n, m);
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Main.DataSuite readDataFromFile(String filename) {
+        Main.DataSuite dataSuite = new Main.DataSuite();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+            String line = bufferedReader.readLine();
+            String[] tokens = line.split(" ");
+            dataSuite.n = Integer.parseInt(tokens[0]);
+            dataSuite.m = Integer.parseInt(tokens[1]);
+            dataSuite.k = Integer.parseInt(tokens[2]);
+            dataSuite.F = new int[dataSuite.n][dataSuite.m];
+            dataSuite.C = new int[dataSuite.k][dataSuite.k];
+            readMatrixFromFile(bufferedReader, dataSuite.F, dataSuite.n, dataSuite.m);
+            readMatrixFromFile(bufferedReader, dataSuite.C, dataSuite.k, dataSuite.k);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataSuite;
+    }
+
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 100;
+
+        generateFile(10, 10, 3, min, max);
+        generateFile(1000, 1000, 5, min, max);
+        generateFile(10, 10000, 5, min, max);
+        generateFile(10000, 10, 5, min, max);
+        generateFile(10000, 10000, 5, min, max);
+    }
+}
